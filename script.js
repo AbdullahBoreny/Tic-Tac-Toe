95019a99f240e92edb53dde644bd24be55b3a3f5
AbdullahBoreny <abdullah55boreny@gmail.com>
2025-02-20 16:38:38 +0300

    remove available cells

--- script.js
+++ script.js
@@ -1,77 +1,128 @@
+/*
+** The Gameboard represents the state of the board
+** Each equare holds a Cell (defined later)
+** and we expose a dropToken method to be able to add Cells to squares
+*/
 
-const gameBoard = (function() {
-  let row = 3;
-  let column = 3;
-  let val = 0;
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
   const board = [];
-  for (let i =0; i<row; i++) {
+
+  // Create a 2d array that will represent the state of the game board
+  // For this 2d array, row 0 will represent the top row and
+  // column 0 will represent the left-most column.
+  // This nested-loop technique is a simple and common way to create a 2d array.
+  for (let i = 0; i < rows; i++) {
     board[i] = [];
-      for (let j = 0; j<column;j++ ) {
-        board[i].push(cell());
-      }
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
   }
-  const dropMark = (col,rows,player)=> {
-    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
-    board[row][col].addMark(player);
-  }
-  const getBoard = ()=> board;
+
+  
+  const getBoard = () => board;
+
+  const dropToken = (row,column, player) => {
+
+    
+    board[row][column].addToken(player);
+  };
+
+  
   
   const printBoard = () => {
     const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
     console.log(boardWithCellValues);
   };
-  
 
-  return { getBoard,printBoard,dropMark };
+  // Here, we provide an interface for the rest of our
+  // application to interact with the board
+  return { getBoard, dropToken, printBoard };
+}
 
-  
-})()
-function cell() {
-  let value = '0';
-  
-  const addMark = (player) => {
+
+
+function Cell() {
+  let value = 0;
+
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
     value = player;
-  }
-  const getValue = ()=> value;
-  return { getValue,addMark };
-  }
+  };
 
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
 
- function gameController(playerOneName="Player One",playerTwoName ="Player Two") {
-    const players = [
-      {
-        name:"playerOneName",
-        mark:'X'
-      },
-      {
-        name:"playerTwoName",
-        mark:'O'
-      }
-    ];
-    let activePlayer = players[0];
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-    const getActivePlayer = ()=> {
-      activePlayer;
+  return {
+    addToken,
+    getValue
+  };
+}
+
+/* 
+** The GameController will be responsible for controlling the 
+** flow and state of the game's turns, as well as whether
+** anybody has won the game
+*/
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: 'X'
+    },
+    {
+      name: playerTwoName,
+      token: 'O'
     }
-    const printNewRound = ()=> {
-      gameBoard.printBoard();
-      console.log(`${getActivePlayer().name}'turn.`);
-    };
+  ];
 
-    const playRound = (row,column) =>{
-       console.log(`Dropping ${getActivePlayer().name}'s token into column ${column} and row ${row}...`
-       );
-       gameBoard.dropMark(1,1,getActivePlayer().mark);
+  let activePlayer = players[0];
 
-       switchPlayerTurn();
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row,column) => {
+    // Drop a token for the current player
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${column} row ${row}...`
+    );
+    board.dropToken(row,column, getActivePlayer().token);
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    // Switch player turn
+    switchPlayerTurn();
     printNewRound();
-    };
-    printNewRound();
+   
+  };
+
+  // Initial play game message
   
-    return { switchPlayerTurn,playRound,getActivePlayer, printNewRound }
-  }
-const game = gameController();
-  
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer
+  };
+}
+
+const game = GameController();
+game.playRound(1,1);
+game.playRound(1,1);
+
 
